426a1ac175f1d200acb2a4a4c4be7173dc086475
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2024-05-09 09:33:00 +0100

    feat: Move Next app into src directory, add README to root directory

---
+++ src/src/app/page.jsx
@@ -0,0 +1,14 @@
+const page = () => {
+  return (
+    <div className="p-4 mt-10 text-center">
+      <h1 className="my-2 text-3xl font-bold md:text-5xl">
+        Hello world, welcome to <span className="text-blue-600">TrackOrg</span>
+      </h1>
+      <p className="text-sm md:text-lg">
+        Automate your organization's register/attendance system with ease.
+      </p>
+    </div>
+  );
+};
+
+export default page;
