7c310aa49b9ce6b9500757b826a4681815e3fcd9
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2024-06-05 12:30:03 +0100

    feat

---
+++ src/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="p-5">
+      <p className="text-center">
+        &copy; {new Date().getFullYear()} TrackOrg. All rights reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
